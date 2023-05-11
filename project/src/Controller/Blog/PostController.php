b849700036be462a473aa8deb5df73d3453f2b28
Adrien Colonna <adriendu13400@gmail.com>
2023-05-11 14:18:08 +0200

    Init project

---
+++ project/src/Controller/Blog/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Blog;
+
+use App\Repository\Post\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'app_post', methods: ['GET'])]
+    public function index(PostRepository $postRepository, Request $request): Response
+    {
+        return $this->render('pages/blog/index.html.twig', [
+            'posts' => $postRepository->findPublished($request->query->getInt('page', 1))
+        ]);
+    }
+}
---
+++ project/src/Repository/Post/PostRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repository\Post;
+
+use App\Entity\Post\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+
+/**
+ * @extends ServiceEntityRepository<Post>
+ *
+ * @method Post|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Post|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Post[]    findAll()
+ * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry, private PaginatorInterface $paginatorInterface)
+    {
+        parent::__construct($registry, Post::class);
+    }
+
+    /**
+     * Get published posts
+     *
+//     * @param int $page
+//     * @param ?Category $category
+//     * @param ?Tag $tag
+//     *
+//     * @return PaginationInterface
+     */
+    public function findPublished(int $page/*, ?Category $category = null, ?Tag $tag = null*/): PaginationInterface
+    {
+        $data = $this->createQueryBuilder('p')
+            ->where('p.state LIKE :state')
+            ->setParameter('state', '%STATE_PUBLISHED%')
+            ->addOrderBy('p.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+//        if (isset($category)) {
+//            $data = $data
+//                ->join('p.categories', 'c')
+//                ->andWhere(':category IN (c)')
+//                ->setParameter('category', $category);
+//        }
+//
+//        if (isset($tag)) {
+//            $data = $data
+//                ->join('p.tags', 't')
+//                ->andWhere(':tag IN (t)')
+//                ->setParameter('tag', $tag);
+//        }
+
+//        $data->getQuery()
+//            ->getResult();
+
+        $posts = $this->paginatorInterface->paginate($data, $page, 9);
+
+        return $posts;
+    }
+
+}
+
