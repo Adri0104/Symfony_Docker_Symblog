122aad2646176425e79d638ac5d4cba52834906c
Adrien Colonna <adriendu13400@gmail.com>
2023-05-11 17:18:53 +0200

    Er

--- project/src/Controller/Blog/PostController.php
+++ project/src/Controller/Blog/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Blog;
 
+use App\Entity\Post\Post;
 use App\Repository\Post\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,4 +18,31 @@ class PostController extends AbstractController
             'posts' => $postRepository->findPublished($request->query->getInt('page', 1))
         ]);
     }
+
+    #[Route('/article/{slug}', name: 'blog.show', methods: ['GET', 'POST'])]
+    public function show(Post $post/*, Request $request, EntityManagerInterface $em*/): Response
+    {
+//        $comment = new Comment();
+//        $comment->setPost($post);
+//        if($this->getUser()) {
+//            $comment->setAuthor($this->getUser());
+//        }
+//
+//        $form = $this->createForm(CommentType::class, $comment);
+//        $form->handleRequest($request);
+//        if($form->isSubmitted() && $form->isValid()) {
+//            $em->persist($comment);
+//            $em->flush();
+//
+//            $this->addFlash('success', 'Votre commentaire a bien été enregistré. Il sera soumis à modération dans les plus brefs délais.');
+//
+//            return $this->redirectToRoute('post.show', ['slug' => $post->getSlug()]);
+//        }
+
+        return $this->render('pages/blog/show.html.twig', [
+            'post' => $post,
+//            'form' => $form->createView()
+        ]);
+    }
+
 }
